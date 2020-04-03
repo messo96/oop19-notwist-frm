6247874769af016c8cfc7a65bd038634dc887055
Giovanni <giovanni.messina3@studio.unibo.it>
2020-04-03 10:44:12 +0200

    Added base class of database management

---
+++ src/main/java/notwist/database/DBManagerImpl.java
@@ -0,0 +1,52 @@
+package notwist.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+abstract class DBManagerImpl{
+
+	private Connection conn;
+    private Statement stmt;
+
+    public DBManagerImpl() {
+    	try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    		open();
+		} 
+		catch (ClassNotFoundException e) {
+			e.getStackTrace();
+		}
+    	
+    }
+	
+	public Statement open() {
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/7lncuOhnfh","7lncuOhnfh", "DLmbbVHESb");
+			stmt = conn.createStatement();
+			System.out.print("Database is connected !");
+			return stmt;
+		} catch (SQLException e) {
+			System.out.println("Error while connect with database"+e);
+		}
+		return null;
+	}
+	
+	public void close() {
+		try {
+			conn.close();
+			System.out.println("Connection closed safely");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	public Connection getConn() {
+		return conn;
+	}
+	
+
+}
