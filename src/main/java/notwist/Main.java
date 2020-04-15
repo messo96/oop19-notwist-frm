aba58fbd1975df796e7b025145a7e33feb76d36f
Giovanni <giovanni.messina3@studio.unibo.it>
2020-04-15 14:15:35 +0200

    Resolve problem git

--- src/main/java/notwist/Main.java
+++ src/main/java/notwist/Main.java
@@ -5,6 +5,8 @@
  */
 package notwist;
 
+import emily.notwist.master_gui;
+
 /**
  *
  * @author gio
@@ -15,7 +17,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    	master_gui gui = new master_gui();
+    	
+    	gui.start();
     }
     
 }
