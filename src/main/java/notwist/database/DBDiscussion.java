85ca902e32d66b1be7070b40f6f6b740ff2fb601
Giovanni <giovanni.messina3@studio.unibo.it>
2020-04-22 11:15:31 +0200

    Added interface DBDiscussion

--- src/main/java/notwist/database/DBDiscussion.java
+++ src/main/java/notwist/database/DBDiscussion.java
@@ -1,70 +1,33 @@
 package notwist.database;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
-public class DBDiscussion extends DBManagerImpl{
-
+public interface DBDiscussion {
 	
-	private ResultSet rs = null;
-    private String query;
 	/**
+	 * Search discussion/s of the user
 	 * 
 	 * @param idUser
 	 * 			identifier of user that create the return discussion
 	 * @return
-	 * 			null if user hasn't create a discussion, the discussion otherwise
+	 * 			Optional empty if user hasn't create a discussion, Optional list of discussions otherwise
 	 */
-	public List<Discussion> getDiscussion(User user) {
-		List<Discussion> discussion = new LinkedList<>();
-		
-		try {
-			query = "select * from DISCUSSION";
-			rs = open().executeQuery(query);
-		 
-	              
-	     while (rs.next()) {
-	    	 if(rs.getInt("id_creator") == (user.getId()))
-	       	discussion.add(new Discussion(user.getId(),rs.getString("title"), rs.getString("description")));
-	     }
-		}
-	     catch(SQLException e) {
-	    	 System.out.println("Error while download discussion"+e);
-	     }
-		finally {
-			close();
-		}
-
-		return discussion;
-		
-	}
+	public Optional< List<Discussion> >  getDiscussion(final User user);
 	
-	public boolean createDiscussion(User user, Discussion discussion, Category topic) {
-				
-		 try {
-			 
-			 	PreparedStatement prepared = super.getConn()
-		        		.prepareStatement("insert into DISCUSSION (id_creator,title,description,id_macro) values (?,?,?,?)");
-		     	prepared.setInt(1,user.getId());
-		        prepared.setString(2, discussion.getTitle());
-		     	prepared.setString(3, discussion.getDescription());
-		     	prepared.setInt(4,topic.getId());
-		     	
-		     	prepared.executeUpdate();
-		     	System.out.println("Discussion create successfully(" + discussion.getTitle() + " | " + user.getName());
-		     	return true;
-		}
-		catch(Exception e) {
-			System.out.println("\nError while adding new user " + e);
-			return false;
-		}
-		finally {
-			close();
-		}
-	}
+	/**
+	 * Create a new discussion and load on database
+	 * 
+	 * @param user
+	 * 			Creator of the discussion
+	 * @param discussion
+	 * 			Discussion to load
+	 * @param topic
+	 * 			Category of the discussion
+	 * @return
+	 */
+	public boolean createDiscussion(final User user, final Discussion discussion, final Category topic);
+		
+	public Optional<List<Discussion>> getAllDiscussion();
 
-	
 }
