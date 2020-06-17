c4e7ecc6301398e1c51bf03b2390d2de7e860a14
Giovanni <giovanni.messina3@studio.unibo.it>
2020-06-17 12:28:49 +0200

    Separated model from the controller Database

--- src/main/java/model/database/DBLikeDislike.java
+++ src/main/java/model/database/DBLikeDislike.java
@@ -1,18 +1,114 @@
 package model.database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface DBLikeDislike{
+public class DBLikeDislikeImpl extends DBManagerImpl implements DBLikeDislike {
+	private class LikeSet {
+		Boolean like;
+		Boolean dislike;
+		Integer idUser;
+
+		LikeSet(final Boolean isLike, final Boolean isDislike, final Integer idUser) {
+			this.like = isLike;
+			this.dislike = isDislike;
+			this.idUser = idUser;
+		}
+
+		public Boolean getLike() {
+			return this.like;
+		}
+
+		public Boolean getDislike() {
+			return this.dislike;
+		}
+
+		public Integer getIdUser() {
+			return this.idUser;
+		}
+
+	}
+
+	private String query;
+	private PreparedStatement prepared;
+	private ResultSet rs;
+
+	private Optional<List<LikeSet>> getDataBase(final Integer idDiscussion) {
+		List<LikeSet> list = new LinkedList<>();
+		try {
+			query = "select * from LIKES where idDiscussion= " + idDiscussion;
+			rs = open().executeQuery(query);
+			while (rs.next()) {
+				list.add(new LikeSet(rs.getBoolean("isLike"), rs.getBoolean("isDislike"), rs.getInt("idUser")));
+			}
+			return Optional.of(list);
+		} catch (SQLException e) {
+			System.out.println("Error while delete comment" + e);
+			return Optional.empty();
+		} finally {
+			close();
+		}
+	}
 	
-	public boolean addLike(final Integer idDiscussion, final Integer idUser);
+	private boolean setDataBase(final Integer idDiscussion, final Integer idUser, final boolean isLike, final boolean isDislike) {
+		try {
+
+			query = "insert into LIKES (idUser, idDiscussion, isLike, isDislike) values (?,?,?,?)";
+			open();
+			prepared = super.getConn().prepareStatement(query);
+			prepared.setInt(1, idUser);
+			prepared.setInt(2, idDiscussion);
+			prepared.setBoolean(3, isLike);
+			prepared.setBoolean(4, isDislike);
+			
+			prepared.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			System.out.println("\nError while adding new comment in discussion " + e);
+			return false;
+		} finally {
+			close();
+		}
+	}
+
+	@Override
+	public Integer getLike(Integer idDiscussion) {
+		return this.getDataBase(idDiscussion).get().stream().filter(s -> s.getLike()).collect(Collectors.toList())
+				.size();
+	}
+
+	@Override
+	public Integer getDislike(Integer idDiscussion) {
+		return this.getDataBase(idDiscussion).get().stream().filter(s -> s.getDislike()).collect(Collectors.toList())
+				.size();
+	}
+
+	@Override
+	public boolean isStillLiked(Integer idDiscussion, Integer idUser) {
+		return !this.getDataBase(idDiscussion).get().stream().filter(s -> s.getLike() && s.getIdUser() == idUser).collect(Collectors.toList()).isEmpty();
+	}
+
+	@Override
+	public boolean isStillDisliked(Integer idDiscussion, Integer idUser) {
+		return !this.getDataBase(idDiscussion).get().stream().filter(s -> s.getDislike() && s.getIdUser() == idUser).collect(Collectors.toList()).isEmpty();
+
+	}
+
+	@Override
+	public boolean addLike(Integer idDiscussion, Integer idUser) {
+		return this.setDataBase(idDiscussion, idUser, true, false);
+	}
+
+	@Override
+	public boolean addDislike(Integer idDiscussion, Integer idUser) {
+		return this.setDataBase(idDiscussion, idUser, false, true);
+	}
+
 	
-	public boolean addDislike(final Integer idDiscussion, final Integer idUser);
-	
-	public Integer getLike(final Integer idDiscussion);
-	
-	public Integer getDislike(final Integer idDiscussion);
-	
-	public boolean isStillLiked(final Integer idDiscussion, final Integer idUser);
-	
-	public boolean isStillDisliked(final Integer idDiscussion, final Integer idUser);
-	
+
 }
--- src/main/java/model/database/DBStrike.java
+++ src/main/java/model/database/DBStrike.java
@@ -1,32 +1,91 @@
 package model.database;
 
-public interface DBStrike {
-
-	/**
-	 * 
-	 * @param idUser id of the user to get Strikes
-	 * @return
-	 * 			number of strikes
-	 */
-	public Integer getStrikes(final Integer idUser);
-	
-	/**
-	 * 
-	 * @param idUser id of the user to change own strikes
-	 * @return
-	 * 			True if the change completely successfully, false otherwise
-	 */
-	public boolean setStrike(final Integer idUser, final Integer value);
-	
-	
-	/**
-	 * 
-	 * @param idUser 
-	 * @return
-	 * 			True if Strike is equal to 3 and can reset, false if not equal and then cannot reset Strike
-	 */
-	public boolean resetStrike(final Integer idUser);
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
-	
+public class DBStrikeImpl extends DBManagerImpl implements DBStrike {
+	private String query;
+	private ResultSet rs;
+	private final Integer MAX_STRIKE = 3;
+
+	@Override
+	public Integer getStrikes(Integer idUser) {
+
+		try {
+			query = "select strike from UTENTE where idUser= " + idUser;
+			rs = open().executeQuery(query);
+
+			if (rs.next()) {
+				return rs.getInt("strike");
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Error while download strike from idUser:" + idUser + "\n" + e);
+		} finally {
+			close();
+		}
+		return -1;
+	}
+
+	@Override
+	public boolean setStrike(final Integer idUser, final Integer value) {
+		try {
+			open();
+			PreparedStatement prepared = getConn()
+					.prepareStatement("update UTENTE set strike = ? where idUser = ?");
+
+			prepared.setInt(1, value);
+			prepared.setInt(2, idUser);
+			prepared.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			System.out.println("Error while increment strike from idUser:" + idUser + "\n" + e);
+			return false;
+		}
+		finally {
+			close();
+		}
+
+	}
+
+	@Override
+	public boolean resetStrike(Integer idUser) {
+		try {
+			if (maxStrike(idUser)) {
+				open();
+				PreparedStatement prepared = getConn()
+						.prepareStatement("update UTENTE set strike = 0 where idUser = ?");
+
+				prepared.setInt(1, idUser);
+				prepared.executeUpdate();
+				return true;
+			} else
+				return false;
+		} catch (SQLException e) {
+			System.out.println("Error while increment strike from idUser:" + idUser + "\n" + e);
+			return false;
+		} finally {
+			close();
+		}
+	}
+
+	private boolean maxStrike(final Integer idUser) {
+		try {
+			query = "select strike from UTENTE where idUser= " + idUser;
+			rs = open().executeQuery(query);
+
+			if (rs.next() && rs.getInt("strike") >= MAX_STRIKE)
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			System.out.println("Error while control if strike is max from idUser:" + idUser + "\n" + e);
+			return false;
+		} finally {
+			close();
+		}
+	}
+
 }
